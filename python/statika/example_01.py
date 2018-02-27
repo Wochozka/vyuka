ed1a74561515f2853219a477de5dfcc09dc8b2f9
David Švarc <david@svarc.it>
2018-02-27 10:21:57 +0100

    Add static variables and static method.

--- python/statika/example_01.py
+++ python/statika/example_01.py
@@ -2,10 +2,16 @@
 # -*- coding: utf-8 -*-
 
 class User:
+
+  minPassLength = 6   # this is static variable
+  nextID = 1          # this is static variable too :-)
+
   def __init__(self, username, password):
     self.username = username
     self.password = password
     self.logged = False
+    self.ID = User.nextID
+    User.nextID += 1
 
   def login(self, password):
     if self.password == password:
